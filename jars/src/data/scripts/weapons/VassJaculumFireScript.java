39a599978717aae4eb5da1907ffcebec33e9ef2d
Nicke535 <nicke@lofwenberg.com>
2019-02-04 18:05:00 +0100

    More cleanup

--- jars/src/data/scripts/weapons/VassJaculumFireScript.java
+++ jars/src/data/scripts/weapons/VassJaculumFireScript.java
@@ -4,9 +4,7 @@ import com.fs.starfarer.api.Global;
 import com.fs.starfarer.api.combat.*;
 import com.fs.starfarer.api.graphics.SpriteAPI;
 import data.scripts.plugins.VassSpriteRenderManager;
-import data.scripts.utils.VassFakeBeam;
 import org.lazywizard.lazylib.MathUtils;
-import org.lazywizard.lazylib.VectorUtils;
 import org.lwjgl.util.vector.Vector2f;
 
 import java.awt.*;
