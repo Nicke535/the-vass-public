85c63b24b6f74d80e2e0188a1ce2ed27494c7c06
Nicke535 <nicke@lofwenberg.com>
2020-08-01 23:37:30 +0200

    Accel and (little) multa stuff

---
+++ jars/src/data/missions/accel_elites_vs_random/MissionDefinition.java
@@ -0,0 +1,24 @@
+package data.missions.accel_elites_vs_random;
+
+import com.fs.starfarer.api.mission.MissionDefinitionAPI;
+import data.missions.VassBaseRandomMissionDefinition;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MissionDefinition extends VassBaseRandomMissionDefinition
+{
+	@Override
+	public void defineMission(MissionDefinitionAPI api)
+	{
+		chooseFactions("vass_accel", null);
+		super.defineMission(api);
+	}
+
+	@Override
+	protected List<String> getForcedPlayerHullmods() {
+		List<String> hullmods = new ArrayList<>();
+		hullmods.add("vass_dummymod_accel_membership");
+		return hullmods;
+	}
+}
