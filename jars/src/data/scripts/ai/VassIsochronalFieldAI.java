bc8a2f3b2af2d09ff0fb4342820e999e15b3d54a
Nicke535 <nicke@lofwenberg.com>
2021-01-14 12:19:43 +0100

    Hopefully fixed Isochronal Field AI

--- jars/src/data/scripts/ai/VassIsochronalFieldAI.java
+++ jars/src/data/scripts/ai/VassIsochronalFieldAI.java
@@ -2,9 +2,12 @@ package data.scripts.ai;
 
 import com.fs.starfarer.api.Global;
 import com.fs.starfarer.api.combat.*;
+import com.fs.starfarer.api.loading.WeaponSpecAPI;
 import com.fs.starfarer.api.util.IntervalUtil;
 import data.scripts.shipsystems.VassIsochronalField;
 import data.scripts.shipsystems.VassTemporalRetreat;
+import org.lazywizard.lazylib.MathUtils;
+import org.lazywizard.lazylib.combat.AIUtils;
 import org.lazywizard.lazylib.combat.CombatUtils;
 import org.lwjgl.util.vector.Vector2f;
 
@@ -15,12 +18,13 @@ import java.util.List;
  * @author Nicke535
  */
 public class VassIsochronalFieldAI implements ShipSystemAIScript {
+    private static final float HIGH_FLUX_THRESHOLD = 0.8f;
+
     //Internal varibles
     private ShipSystemAPI system;
     private ShipAPI ship;
 
-    //Checks quite often for a good activation time
-    private final IntervalUtil tracker = new IntervalUtil(0.13f, 0.15f);
+    private final IntervalUtil tracker = new IntervalUtil(0.7f, 1.5f);
 
 
     @Override
@@ -38,9 +42,35 @@ public class VassIsochronalFieldAI implements ShipSystemAIScript {
             //First, check if we're in defensive or offensive mode. This will affect our decisionmaking down the line
             boolean offensiveMode = Global.getCombatEngine().getCustomData().containsKey(VassIsochronalField.OFFENSE_MEM_KEY);
 
-            boolean desiresOffensive = false;
+            boolean desiresOffensive = true;
 
-            //TODO: IMPLEMENT
+            //First off: are we running away, or at too high flux? In that case, just directly switch to defensive
+            if (ship.getFluxLevel() > HIGH_FLUX_THRESHOLD
+                    || ship.getAIFlags().hasFlag(ShipwideAIFlags.AIFlags.RUN_QUICKLY)
+                    || ship.isRetreating()
+                    || ship.isDirectRetreat()) {
+                desiresOffensive = false;
+            }
+
+            //Second: are a majority of our applicable weapons disabled? If so, also go on the defensive: we get more mileage out of it
+            int weaponsDisabled = 0;
+            for (WeaponAPI wep : ship.getDisabledWeapons()) {
+                if (wep.getSlot().isHardpoint()) {
+                    weaponsDisabled++;
+                }
+            }
+            if (weaponsDisabled >= 2) {
+                desiresOffensive = false;
+            }
+
+            //Third, are there any targets nearby? If not, go on the defensive
+            //      This is the heaviest check, so we don't even bother checking if we know we should be on the defensive
+            if (desiresOffensive) {
+                ShipAPI enemy = AIUtils.getNearestEnemy(ship);
+                if (enemy == null || MathUtils.getDistance(enemy, ship) > getLongestWeaponRange()*1.25f) {
+                    desiresOffensive = false;
+                }
+            }
 
             if (desiresOffensive && !offensiveMode) {
                 activateSystem();
@@ -50,15 +80,24 @@ public class VassIsochronalFieldAI implements ShipSystemAIScript {
         }
     }
 
-    private void deactivateSystem() {
-        if (system.isOn()) {
-            ship.useSystem();
-        }
-    }
-
     private void activateSystem() {
         if (!system.isOn()) {
             ship.useSystem();
         }
+    }
+
+    //Shorthand for getting the longest applicable weapon range on our ship
+    private float getLongestWeaponRange() {
+        float maxRange = 0f;
+        for (WeaponAPI wep : ship.getAllWeapons()) {
+            if (!wep.getSlot().isHardpoint()) {
+                continue;
+            }
+
+            if (maxRange < wep.getRange()) {
+                maxRange = wep.getRange();
+            }
+        }
+        return maxRange;
     }
 }
